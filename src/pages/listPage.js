7b14b71059d7c3113f0427562cb8426a53d413ec
Cactice <14835424+Cactice@users.noreply.github.com>
2018-12-01 21:59:08 +0900

    experiment getinitialpros

--- src/pages/listPage.js
+++ src/pages/listPage.js
@@ -2,34 +2,69 @@ import Header from '../components/Header'
 import Layout from '../components/MyLayout.js'
 import Head from 'next/head'
 
+import axios from 'axios';
 
 
 var halt = 'alkdjf'
 export default class Example extends React.Component {
 
+  static async getInitialProps () {
+    // res is assigned the response once the axios
+    // async get is completed
+    const res = await axios.get('http://api.football-data.org/v1/competitions/426/leagueTable');
+    // Return properties
+    return {data: res.data}
+  }
   constructor(props, context) {
     super(props, context);
   }
+
   render () {
     return (
       <div>
-      <Layout>
-          <p>{halt}</p>
-        </Layout>
+        <Head>
+            <title>League Table</title>
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            <link rel="stylesheet" href="https://unpkg.com/purecss@0.6.1/build/pure-min.css" />
+        </Head>
+        <div className="pure-g">
+            <div className="pure-u-1-3"></div>
+            <div className="pure-u-1-3">
+              <h1>Barclays Premier League</h1>
+              <table className="pure-table">
+                <thead>
+                  <tr>
+                    <th>Position</th>
+                    <th>Team</th>
+                    <th>P</th>
+                    <th>GL</th>
+                    <th>W</th>
+                    <th>D</th>
+                    <th>L</th>
+                  </tr>
+                </thead>
+                <tbody>
+                {this.props.data.standing.map((standing, i) => {
+                  const oddOrNot = i % 2 == 1 ? "pure-table-odd" : "";
+                  return (
+                      <tr key={i} className={oddOrNot}>
+                        <td>{standing.position}</td>
+                        <td><img className="pure-img logo" src={standing.crestURI}/></td>
+                        <td>{standing.points}</td>
+                        <td>{standing.goals}</td>
+                        <td>{standing.wins}</td>
+                        <td>{standing.draws}</td>
+                        <td>{standing.losses}</td>
+                      </tr>
+                    );
+                })}
+                </tbody>
+              </table>
+            </div>
+            <div className="pure-u-1-3"></div>
+        </div>
       </div>
-    )
+    );
   }
 
-}
-
-
-Example.getInitialProps = async function() {
-  const res  = await fetch('https://api.tvmaze.com/search/shows?q=batman')
-  const data = await res.json()
-
-  console.log(`Show data fetched. Count: ${data.length}`)
-
-  return {
-    shows: data
-  }
 }
